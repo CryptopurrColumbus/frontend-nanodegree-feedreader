b9f455afeca99bcbc781e8b0c1897abb397c4590
Cryptopurr Columbu <cryptopurrcolumbus@gmail.com>
2019-04-19 22:43:05 +0530

    Add name defined test
    Signed-off-by: Cryptopurr Columbu <cryptopurrcolumbus@gmail.com>

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -44,6 +44,13 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('name defined', function() {
+            for(var feed of allFeeds) {
+                expect(feed).toBeDefined();
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe(null);
+            }
+        })
     });
 
 
